1c3a11802fb02e1a90c1b10d3e0a56eee4f40a96
officialozioma <dikep15@gmail.com>
2022-07-12 20:37:04 +0100

    updated vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Complain management system</title>
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -48,7 +48,7 @@
     </div>
 
 
-    @vite('resources/js/app.js')
+    {{-- @vite('resources/js/app.js') --}}
 
 
     <script src="{{ asset('build/assets/app.b0047f3e.js') }}"></script>
